0affb2093a1f5fdbbc06924a5d595fbba319ee5e
a-ru2016 <124528449+a-ru2016@users.noreply.github.com>
2024-01-22 06:39:12 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,15 +1,19 @@
 from .merge.dare import DareModelMerger
+from .merge.dareMBW import DareModelMergerMBW
 from .merge.mag import MagnitudePruningModelMerger
 from .merge.block import BlockModelMergerAdv
 
+
 NODE_CLASS_MAPPINGS = {
     "DareModelMerger": DareModelMerger,
+    "DareModelMergerMBW": DareModelMergerMBW,
     "MagnitudeModelMerger": MagnitudePruningModelMerger,
     "BlockModelMergerAdv": BlockModelMergerAdv,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "DareModelMerger": "ModelMergeByDARE",
+    "DareModelMergerMBW": "ModelMergeByDAREMBW",
     "MagnitudeModelMerger": "ModelMergeByMagnitudePruning",
     "BlockModelMergerAdv": "ModelMergeByBlock (Advanced)",
 }
